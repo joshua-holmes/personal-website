5dadb18cc2309d892889272517795ce252bc9d79
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-06-04 01:06:39 -0500

    Add privacy policy for Renner Family Photos site

--- app/src/App.js
+++ app/src/App.js
@@ -2,30 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import "./libs/font-awesome/css/font-awesome.css";
 
-import Nav from "./components/Nav";
-import Lead from "./components/Lead";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Education from "./components/Education";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import PrivacyRenner from "./components/PrivacyRenner";
 
 function App() {
   
   return (
-    <>
-      <Nav />
-      <Lead />
-      <About />
-      <Experience />
-      <Education />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/privacy/renner-family-photos" element={<PrivacyRenner />} />
+    </Routes>
   );
 }
 
---
+++ app/src/components/Home.js
@@ -0,0 +1,28 @@
+import Nav from "./Nav";
+import Lead from "./Lead";
+import About from "./About";
+import Experience from "./Experience";
+import Education from "./Education";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+function Home() {
+
+    return (
+        <>
+            <Nav />
+            <Lead />
+            <About />
+            <Experience />
+            <Education />
+            <Projects />
+            <Skills />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
